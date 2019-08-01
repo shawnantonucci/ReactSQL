29938ea5234dd213375aceca8d0c1e08116ce98a
shawn <shawnantonucci2018@gmail.com>
2019-07-31 19:54:52 -0700

    updated heroku connection

--- dbconnection.js
+++ dbconnection.js
@@ -1,11 +1,22 @@
 const mysql = require("mysql");
 require('dotenv').config()
 
+// Localhost
+
+// const connection = mysql.createConnection({
+//     host: "localhost",
+//     user: process.env.USER,
+//     password: process.env.PASSWORD,
+//     database: "products"
+// });
+
+// Heroku
+
 const connection = mysql.createConnection({
-    host: "localhost",
-    user: process.env.USER,
-    password: process.env.PASSWORD,
-    database: "products"
+    host: "us-cdbr-iron-east-02.cleardb.net",
+    user: process.env.USER_HEROKU,
+    password: process.env.PASSWORD_HEROKU,
+    database: "heroku_073aa483c0acda9"
 });
 
 connection.connect(err => {
