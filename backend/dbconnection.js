16648f373beda2cdd6a3c5f73404258c579d488a
shawn <shawnantonucci2018@gmail.com>
2019-07-31 18:41:59 -0700

    fixed be

---
+++ backend/dbconnection.js
@@ -0,0 +1,31 @@
+const mysql = require("mysql");
+require('dotenv').config()
+
+// Localhost
+
+const connection = mysql.createConnection({
+    host: "localhost",
+    user: process.env.USER,
+    password: process.env.PASSWORD,
+    database: "products"
+});
+
+// Heroku
+
+// const connection = mysql.createConnection({
+//     host: process.env.DATABASE_HOST,
+//     user: process.env.USER_HEROKU,
+//     password: process.env.PASSWORD_HEROKU,
+//     database: "heroku_073aa483c0acda9"
+// });
+
+connection.connect(err => {
+    if (err) {
+        console.log(err);
+    } else {
+        console.log("Connected to the MySQL server");
+    }
+});
+
+
+module.exports=connection;
